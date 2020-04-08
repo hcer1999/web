60aeaaa21b9f9add3e0fc9a312c34d351676327e
hcer <bingkelele007@163.com>
2020-04-08 23:24:36 +0800

    修复bug

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/tictactoe/dist' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/web/tictactoe/dist' : '/',
 };
